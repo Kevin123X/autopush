4c553f31fc95f499c98989de50de397b92ecd16e
Kevin <liukaiwen-cn@outlook.com>
2020-08-23 18:36:18 +0800

    20200823183551

--- autopush.bat
+++ autopush.bat
@@ -1,7 +1,6 @@
 git add .
 set "timestamp=%date:~,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
-echo «Î ‰»Îcommit –≈œ¢
-set /p input=
+set /p input=Êõ¥Êñ∞Ôºö
 git commit -m  "%timeStamp%%input%"
 git push
 exit
