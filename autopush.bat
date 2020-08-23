43571fcbe482464be5e09f8aef0cfb7b347bf950
Kevin <liukaiwen-cn@outlook.com>
2020-08-23 18:36:29 +0800

    20200823183621

--- autopush.bat
+++ autopush.bat
@@ -1,6 +1,6 @@
 git add .
 set "timestamp=%date:~,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
-set /p input=更新：
+set /p input="更新："
 git commit -m  "%timeStamp%%input%"
 git push
 exit
