d5ece5081dd656143d2c63a43fbb452b7026971d
Kevin <liukaiwen-cn@outlook.com>
2020-08-23 18:33:17 +0800

    20200823182443

--- autopush.bat
+++ autopush.bat
@@ -1,21 +1,6 @@
 git add .
-    set yyyy=%date:~,4%
-    set mm=%date:~5,2%
-    set day=%date:~8,2% 
-    set "YYYYmmdd=%yyyy%%mm%%day%"
-    set "YYYYmmdd=%YYYYmmdd: =0%"
-    echo "YYYYmmdd%YYYYmmdd%YYYYmmdd"
-    set hh=%time:~0,2%
-    set mi=%time:~3,2%
-    set ss=%time:~6,2% 
-    set "hhmiss=%hh%%mi%%ss%"
-    set "hhmiss=%hhmiss: =0%"
-    echo "hhmiss%Time%hhmiss"
-    echo %hhmiss%
-    set "hhmiss=%hhmiss::=0%"
-    set "hhmiss=%hhmiss: =0%"
-    set "timestamp=%YYYYmmdd%%hhmiss%"
-set /p input=¸üÐÂ£º
+set "timestamp=%date:~,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
+set /p input=ï¿½ï¿½ï¿½Â£ï¿½
 git commit -m  "%timeStamp%%input%"
 git push
 exit
